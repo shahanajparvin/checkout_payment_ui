9e613d9c29f6937d8ec03b1f19a0250eba90ee24
putulputul <putulcse9@gmail.com>
2024-05-29 20:05:18 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:checkout_payment_ui/Features/checkout/presentation/views/my_cart_view.dart';
+import 'package:checkout_payment_ui/Features/checkout/presentation/views/payment_details.dart';
+import 'package:checkout_payment_ui/core/utils/api_keys.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
+
+void main() {
+  Stripe.publishableKey = ApiKeys.puplishableKey;
+
+  runApp(const CheckoutApp());
+}
+
+class CheckoutApp extends StatelessWidget {
+  const CheckoutApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+       home:PaymentDetailsView(),
+     // home: MyCartView(),
+    );
+  }
+}
+
+// PaymentIntentModel create payment intent(amount , currency , customerId)
+// keySecret createEphemeralKey( customerId)
+// initPaymentSheet (merchantDisplayName , intentClientSecret , ephemeralKeySecret)
+// presentPaymentSheet()
